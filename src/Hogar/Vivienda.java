88d8a12feda32b13ce290e7031b361d1965fe992
Usuario <Usuario@DESKTOP-AEVPLJ5>
2023-03-17 13:09:31 +0100

    Refactorización realizada!
    Métodos y constructores comentados.

--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -7,11 +7,18 @@ package Hogar;
 
 /**
  *
- * @author paqui
+ * @author SergioQuijada
  */
 public class Vivienda {
 
-    
+    /**
+    * @param precio. Valor real que determina el precio de la vivienda
+    * @param numHabitaciones. Valor entero que determina el numero de habitaciones
+    * @param superficie. Valor real que determina la superficie de la vivienda
+    * @param parking. Valor de tipo booleano que indica si dispone de parking o no
+    * @param estado. String que determina en que estado se encuentra la vivienda
+    * @param propietario. String que determina el propietario de la vivienda
+    */
     private double precio;
     private int numHabitaciones;
     private double superficie;
@@ -30,84 +37,84 @@ public class Vivienda {
     
     
     /**
-     * @return the precio
+     * @return Nos devuelve el precio
      */
     public double getPrecio() {
         return precio;
     }
 
     /**
-     * @param precio the precio to set
+     * @param precio. Valor real que determina el precio de la vivienda
      */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
     /**
-     * @return the numHabitaciones
+     * @return Nos devuelve el numero de habitaciones
      */
     public int getNumHabitaciones() {
         return numHabitaciones;
     }
 
     /**
-     * @param numHabitaciones the numHabitaciones to set
+     * @param numHabitaciones. Valor entero que determina el numero de habitaciones
      */
     public void setNumHabitaciones(int numHabitaciones) {
         this.numHabitaciones = numHabitaciones;
     }
 
     /**
-     * @return the superficie
+     * @return Nos devuelve la superficie
      */
     public double getSuperficie() {
         return superficie;
     }
 
     /**
-     * @param superficie the superficie to set
+     * @param superficie. Valor real que determina la superficie de la vivienda
      */
     public void setSuperficie(double superficie) {
         this.superficie = superficie;
     }
 
     /**
-     * @return the parking
+     * @return Nos devuelve si tiene parking o no
      */
     public boolean isParking() {
         return parking;
     }
 
     /**
-     * @param parking the parking to set
+     * @param parking. Valor de tipo booleano que indica si dispone de parking o no
      */
     public void setParking(boolean parking) {
         this.parking = parking;
     }
 
     /**
-     * @return the estado
+     * @return Nos devuelve el estado
      */
     public String getEstado() {
         return estado;
     }
 
     /**
-     * @param estado the estado to set
+     * @param estado. String que determina en que estado se encuentra la vivienda
      */
     public void setEstado(String estado) {
         this.estado = estado;
     }
 
     /**
-     * @return the propietario
+     * @return Nos devuelve el propietario
      */
     public String getPropietario() {
         return propietario;
     }
 
     /**
-     * @param propietario the propietario to set
+     * @param propietario. String que determina el propietario de la vivienda
      */
     public void setPropietario(String propietario) {
         this.propietario = propietario;
@@ -117,12 +124,14 @@ public class Vivienda {
     
     public Vivienda (double precio)
     {
+        /*Construsctor indicando precio*/
         this.precio = precio;
     }
     
     public void actualizarPrecio(double descuento)
     {
-        setPrecio(getPrecio() - getPrecio() * 0.05);
+        /*Metodo que nos devuelve el precio con el descuento*/
+        setPrecio(getPrecio() - getPrecio() * descuento);
     }
     
 }
